dc8784ab17952f14298637b181c40f2001f91641
Marisa Giancarla <fstltna@yahoo.com>
2023-06-01 15:43:45 -0700

    first checkin

---
+++ setup.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+@startw unpack.exe
+@startw inject.exe "locSophies_Kitchen.dll" "plaMartine.dll" "plaKing_Colin.dll" "plaHoratio.dll" "plaChelsea.dll" "locWonderland.dll" "locAlumni_Room.dll" "locDobrin_Manor.dll" "plaSue.dll" "plaGiant.dll" 
+@startw inject.exe "plaMiMiZu.dll" "plaMerlina.dll" "plaJared.dll" "plaMiniQueen.dll" "locTitanic.dll" "locBeanstalk.dll" "locOn_the_Rocks.dll" "locFreeverse_HQ.dll" "locEgyptian_Excursion.dll" "locDreamland.dll" 
+@startw inject.exe "plaMelon_Mello.dll" "locHaunted_Room.dll" "plaiPuppet_Guy.dll" "plaiPuppet_Gal.dll" "plaZeb.dll" "plaMyles.dll" "plaMonty_Zuma.dll" "plaMei.dll" "plaKate.dll" "plaJet.dll" 
+@startw inject.exe "plaCheeseBot.dll" "plaJen.dll" "plaIan.dll" "plaHal.dll" "plaJeremy.dll" "locHMS_Banana.dll" "locCentral_Park.dll"
+@startw regsetup.exe
+@echo DONE.
